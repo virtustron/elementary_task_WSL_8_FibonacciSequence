e1c0739791dfb299dd15a7d7a4397c2f2ff9843a
virtustron <virtustron@gmail.com>
2021-09-30 08:21:03 +0300

    Add Fibonacci next number calculation

--- src/FibonacciSequenceIterator.cpp
+++ src/FibonacciSequenceIterator.cpp
@@ -3,15 +3,42 @@
 
 FibonacciSequenceIterator::FibonacciSequenceIterator()
 {
-    m_iterator = 1;
-    m_last = 1;
+    m_iterator = 0;
+    m_last = 0;
     m_before_last = 0;
 }
 
 void FibonacciSequenceIterator::Next()
 {
+    // 0 1 1 2 3 5 8 13 21 34
+    
     if (m_iterator == 0)
-    m_iterator++;
+    {   
+        m_iterator++;
+    } 
+
+    if (m_iterator == 1 && m_last == 0 )
+    {   
+        m_last = 1;
+    }  
+
+    if (m_iterator == 1 && m_last == 1 )
+    {   
+        m_iterator += m_last;
+
+        m_before_last = 1;
+        m_last = m_iterator;
+
+    } 
+
+    if (m_iterator > 1)
+    {   
+        m_iterator += m_last + m_before_last;
+
+        m_before_last = m_last;
+        m_last = m_iterator;
+    }
+
 }
 
 bool FibonacciSequenceIterator::IsDone() 
